0f71c7056360e81eb924ec3dfe11c98f7d3c4de3
bnohad <bnohad@gmail.com>
2017-03-13 16:39:53 +0200

    Fixed slack publish token error

--- script/runplaybook.sh
+++ script/runplaybook.sh
@@ -5,11 +5,11 @@ runPlaybook() {
     echo running ansible playbook action: $1
     if [ -z "$1" ]
     then
-      docker run -it --rm -v $PWD:/ansible/playbooks -v ~/.aws:/root/.aws vayuadm/vayu-ansible-client test.yaml
+      docker run -it -v $PWD:/ansible/playbooks -v ~/.aws:/root/.aws vayuadm/vayu-ansible-client test.yaml
     else
-      docker run -it --rm -v $PWD:/ansible/playbooks -v ~/.aws:/root/.aws vayuadm/vayu-ansible-client aws.yaml --tags "$1"
+      docker run -it -v $PWD:/ansible/playbooks -v ~/.aws:/root/.aws vayuadm/vayu-ansible-client aws.yaml --tags "$1"
     fi
-    docker run -it --rm -v $PWD:/ansible/playbooks -v ~/.aws:/root/.aws vayuadm/vayu-ansible-client slack-publish.yaml
+    docker run -it -v $PWD:/ansible/playbooks -v ~/.aws:/root/.aws vayuadm/vayu-ansible-client slack-publish.yaml
 }
 
 runPlaybook $1
