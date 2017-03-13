c00424220a10af934b258d9ed957f70c1335f91a
bnohad <bnohad@gmail.com>
2017-03-13 15:48:47 +0200

    Ansible playbook for slack, updated script for running playbooks

--- script/runplaybook.sh
+++ script/runplaybook.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-
-readonly TAG_DESTROY="destroy"
+# available actions: create, destroy, kops-create, kops-destroy, aws-create, aws-destroy
 
 runPlaybook() {
     echo running ansible playbook action: $1
-    if [ "$1" = "${TAG_DESTROY}" ]
+    if [ -z "$1" ]
     then
-        docker run -it --rm -v $PWD:/ansible/playbooks -v ~/.aws:/root/.aws vayuadm/vayu-ansible-client aws.yaml --tags "${TAG_DESTROY}"
+      docker run -it --rm -v $PWD:/ansible/playbooks -v ~/.aws:/root/.aws vayuadm/vayu-ansible-client test.yaml
     else
-        docker run -it --rm -v $PWD:/ansible/playbooks -v ~/.aws:/root/.aws vayuadm/vayu-ansible-client test.yaml
+      docker run -it --rm -v $PWD:/ansible/playbooks -v ~/.aws:/root/.aws vayuadm/vayu-ansible-client aws.yaml --tags "$1"
     fi
+    docker run -it --rm -v $PWD:/ansible/playbooks -v ~/.aws:/root/.aws vayuadm/vayu-ansible-client slack-publish.yaml
 }
 
 runPlaybook $1
