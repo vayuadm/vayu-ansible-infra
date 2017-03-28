0b9903592c5cf6b128efb7e5b91e5c34d888a9cc
kornfeld <lital.kornfeld@hpe.com>
2017-03-28 11:23:31 +0300

    removed publish to slack, added full path to task includes

--- script/runplaybook.sh
+++ script/runplaybook.sh
@@ -10,12 +10,6 @@ runPlaybook() {
       echo Running ansible playbook action: "$2"
       docker run -it -v $PWD:/ansible/playbooks -v ~/.aws:/root/.aws vayuadm/vayu-ansible-client aws.yaml --tags "$2"
     fi
-
-    if [ ! -z "$1" ]
-    then
-      echo Publishing results to Slack
-      docker run -it -e SLACK_TOKEN="$1" -v $PWD:/ansible/playbooks -v ~/.aws:/root/.aws vayuadm/vayu-ansible-client slack-publish.yaml
-    fi
 }
 
 runPlaybook $1 $2
