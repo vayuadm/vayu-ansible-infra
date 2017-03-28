a797664a32b8dda621b61717be84a9296b77cf2a
kornfeld <lital.kornfeld@hpe.com>
2017-03-28 15:20:36 +0300

    fixed typo

--- script/runplaybook.sh
+++ script/runplaybook.sh
@@ -6,7 +6,7 @@ runPlaybook() {
     then
       echo Running ansible test playbook
       docker run -it -v $PWD:/ansible/playbooks -v ~/.aws:/root/.aws vayuadm/vayu-ansible-client test.yaml
-    elif [[ "$2" == *"create"*]]
+    elif [ "$2" == *"create"*]
     then
       echo Running aws-create.yaml playbook action: "$2"
       docker run -it -v $PWD:/ansible/playbooks -v ~/.aws:/root/.aws vayuadm/vayu-ansible-client aws-create.yaml --tags "$2"
