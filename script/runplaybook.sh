a81ff64ee455b5ffe5355afad4566cb60b99d6b8
kornfeld <lital.kornfeld@hpe.com>
2017-03-28 14:18:31 +0300

    separated create & destroy logic

--- script/runplaybook.sh
+++ script/runplaybook.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
-# available actions: create, destroy, kops-create, kops-destroy, aws-create, aws-destroy
+# available actions: create, destroy, kops-create, kops-destroy, aws-create, aws-destroy, deployment
 
 runPlaybook() {
     if [ -z "$2" ]
     then
       echo Running ansible test playbook
       docker run -it -v $PWD:/ansible/playbooks -v ~/.aws:/root/.aws vayuadm/vayu-ansible-client test.yaml
+    elif [[ "$2" == *"create"*]]
+    then
+      echo Running aws-create.yaml playbook action: "$2"
+      docker run -it -v $PWD:/ansible/playbooks -v ~/.aws:/root/.aws vayuadm/vayu-ansible-client aws-create.yaml --tags "$2"
     else
-      echo Running ansible playbook action: "$2"
-      docker run -it -v $PWD:/ansible/playbooks -v ~/.aws:/root/.aws vayuadm/vayu-ansible-client aws.yaml --tags "$2"
+      echo Running aws-destroy.yaml playbook action: "$2"
+      docker run -it -v $PWD:/ansible/playbooks -v ~/.aws:/root/.aws vayuadm/vayu-ansible-client aws-destroy.yaml --tags "$2"
     fi
 }
 
