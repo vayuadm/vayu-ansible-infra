2a6ec372c7a17c0179d61205708f76dfd4c6a91a
kornfeld <lital.kornfeld@hpe.com>
2017-03-29 09:55:38 +0300

    fixed script

--- script/runplaybook.sh
+++ script/runplaybook.sh
@@ -6,13 +6,13 @@ runPlaybook() {
     then
       echo Running ansible test playbook
       docker run -it -v $PWD:/ansible/playbooks -v ~/.aws:/root/.aws vayuadm/vayu-ansible-client test.yaml
-    elif [[ "$2" == *"create"* ]]
+    elif [[ "$2" == *"destroy"* ]]
     then
-      echo Running aws-create.yaml playbook action: "$2"
-      docker run -it -v $PWD:/ansible/playbooks -v ~/.aws:/root/.aws vayuadm/vayu-ansible-client aws-create.yaml --tags "$2"
-    else
       echo Running aws-destroy.yaml playbook action: "$2"
       docker run -it -v $PWD:/ansible/playbooks -v ~/.aws:/root/.aws vayuadm/vayu-ansible-client aws-destroy.yaml --tags "$2"
+    else
+      echo Running aws-create.yaml playbook action: "$2"
+      docker run -it -v $PWD:/ansible/playbooks -v ~/.aws:/root/.aws vayuadm/vayu-ansible-client aws-create.yaml --tags "$2"
     fi
 }
 
