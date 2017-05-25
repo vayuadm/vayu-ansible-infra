eb863b6dc99d92d8d615ec0a25920cb0751ad2d6
kornfeld <lital.kornfeld@hpe.com>
2017-05-25 10:38:53 +0300

    added missing then...

--- script/runplaybook.sh
+++ script/runplaybook.sh
@@ -11,9 +11,11 @@ runPlaybook() {
       echo Running aws-destroy.yaml playbook action: "$2"
       docker run -it -v $PWD:/ansible/playbooks -v ~/.aws:/root/.aws vayuadm/vayu-ansible-client aws-destroy.yaml --tags "$2"
     elif [["$2" == *"create"*]]
+    then
       echo Running aws-create.yaml playbook action: "$2"
       docker run -it -v $PWD:/ansible/playbooks -v ~/.aws:/root/.aws vayuadm/vayu-ansible-client aws-create.yaml --tags "$2"
     elif [["$2" == *"poc"*]]
+    then
       echo Running poc.yaml playbook action: "$2"
       docker run -it -v $PWD:/ansible/playbooks -v ~/.aws:/root/.aws vayuadm/vayu-ansible-client poc.yaml --tags "$2"
     fi
